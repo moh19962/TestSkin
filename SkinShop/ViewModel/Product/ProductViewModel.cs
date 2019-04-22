a2d2f41f369c1df48bf381110b934201f129db05
Mohammad <m.parwani@student.fontys.nl>
2019-04-22 15:00:04 +0200

    View van Product aangemaakt

---
+++ SkinShop/ViewModel/Product/ProductViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkinShop.ViewModel.Product
+{
+    public class ProductViewModel
+    {
+        public int ProductType { get; set; }
+        public string Description { get; set; }
+        public Model.Product product { get; set; }
+        //public List<Model.Product> Products { get; set; } = new List<Model.Product>();
+        public IFormFile Image { get; set; }
+
+        public ProductViewModel() { }
+
+        public ProductViewModel(int productType)
+        {
+            product = new Model.Product();
+            ProductType = productType;
+        }
+    }
+}
