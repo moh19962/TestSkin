b38120606a2255dc4fa9673d7a15c90e016a1be4
Mohammad <m.parwani@student.fontys.nl>
2019-05-05 20:46:21 +0200

    Winkelwagen lagen aangemaakt

---
+++ Model/Cart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Cart
+    {
+        public int CartID { get; set; }
+        public int UserID { get; set; }
+        public Product Product { get; set; }
+        public int Amount { get; set; }
+        public int SubTotal { get; set; }
+        public DateTime Date { get; set; }
+
+        public Cart()
+        {
+            Product = new Product();
+        }
+    }
+}
---
+++ SkinShop/ViewModel/Cart/CartViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Killer.ViewModels.Cart
+{
+    public class CartViewModel
+    {
+        public Model.Product Product { get; set; } = new Model.Product();
+        public List<Model.Product> Producten = new List<Model.Product>();
+        public List<Model.Cart> Cart { get; set; } = new List<Model.Cart>();
+        public List<Model.Cart> Carts { get; set; } = new List<Model.Cart>();
+        public Model.Cart cart { get; set; } = new Model.Cart();
+        //public Model.Order order { get; set; } = new Model.Order();
+
+
+        public CartViewModel()
+        {
+            Cart = new List<Model.Cart>();
+            Carts = new List<Model.Cart>();
+        }
+    }
+}
